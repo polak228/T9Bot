b508b0f924a325ecd60f49993af907e063385246
polak228 <zharov.roman54@gmail.com>
2022-04-04 23:38:27 +0600

    init

---
+++ core.py
@@ -0,0 +1,38 @@
+#-*- coding: utf-8 -*-
+from os import environ
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from pyaspeller import YandexSpeller
+from re import sub, compile
+from gc import collect
+
+class T9Bot(object):
+    def __init__(self, config : dict):
+        self.blanks = config['blanks']
+        self.engine = Bot(environ['TOKEN'], parse_mode = types.ParseMode.HTML)
+        self.dispatcher = Dispatcher(self.engine)
+        self.speller = YandexSpeller()
+
+    def __spelled(self, message : str):
+        result = self.blanks['correctly']
+        try: corrected = self.speller.spelled(message)
+        except: result = self.blanks['error']
+        if(message != corrected): result = self.blanks['incorrectly'].format(corrected)
+        return result
+
+    def __init_handlers(self):
+        @self.dispatcher.message_handler(commands = ['start', 'help'])
+        async def process_command(message : types.Message):
+            response = self.blanks[message.text.replace('/', '')]
+            await self.engine.send_message(message.from_user.id, response)
+        @self.dispatcher.message_handler()
+        async def process_message(message : types.Message):
+            response = self.__spelled(sub(compile('<.*?>') , '', message.text))
+            await message.reply(response)
+
+    def run(self):
+        self.__init_handlers()
+        try: executor.start_polling(self.dispatcher)
+        except Exception as error: print(f'Error: {error}')
+        finally: (collect(), self.run())
---
+++ index.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+from core import T9Bot
+from json import load
+
+if(__name__ == '__main__'):
+    config = {}
+    with open('config.json', 'r', encoding = 'utf-8') as string:
+        config = load(string)
+    T9Bot(config).run()
